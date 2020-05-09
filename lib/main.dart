f74588182ce14be3e7c19611871d56fa5889c3cf
MADAN KUMAR <madankumar051@gmail.com>
2020-05-09 10:31:16 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+import 'package:audioplayers/audio_cache.dart';
+
+void main() => runApp(XylophoneApp());
+
+class XylophoneApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Center(
+            child: FlatButton(
+                onPressed: () {
+                  final player = AudioCache();
+                  player.play('note2.wav');
+                },
+                child: Text('click me !')),
+          ),
+        ),
+      ),
+    );
+  }
+}
